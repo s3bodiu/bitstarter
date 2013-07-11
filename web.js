2a7f78083121f07b7c0b93ec1487cbea95de7870
s3bodiu <s3bodiu@gmail.com>
2013-07-11 23:12:11 +0800

    Read index from fs

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
-
+var fs = require('fs');
+var buffer;
+var text = fs.readFile('index.html', function (err, data) {
+    if (err) throw err;
+    buffer = data;
+});
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
